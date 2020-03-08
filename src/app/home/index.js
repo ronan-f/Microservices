0477faeef86708127b8174aac3cdb7242515b8c7
Ronan Fegan <rfegan13@gmail.com>
2020-03-07 17:27:40 -0800

    Added funcitons to handle home route

---
+++ src/app/home/index.js
@@ -0,0 +1,37 @@
+const camelCaseKeys = require('camelcase-keys');
+const express = require('express');
+
+function createHandlers({ queries }) {
+    async function home(req, res, next) {
+        try {
+            const viewData = await queries.loadHomePage();
+
+            return res.render('home/templates/home', viewData);
+        } catch (e) {
+            next();
+        }
+    }
+    return { home };
+}
+
+function createQueries({ db }) {
+    async function loadHomePage() {
+        const client = await db;
+        const result = client('videos').sum('view_count as videosWatched');
+        return result[0];
+    }
+    return { loadHomePage };
+}
+
+function createHome({ db }) {
+    const queries = createQueries({ db });
+    const handlers = createHandlers(queries);
+
+    const router = express.router();
+
+    router.route('/').get(handlers.home);
+
+    return { handlers, queries, router };
+}
+
+module.exports = createHome;
