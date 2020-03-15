c2dd3b0d6a6a015d32a110e1224738276e05f304
Ronan Fegan <rfegan13@gmail.com>
2020-03-14 18:10:02 -0700

    Updated config to take databaseUrl from env

--- src/env.js
+++ src/env.js
@@ -24,5 +24,6 @@ module.exports = {
     appName: requireFromEnv('APP_NAME'),
     env: requireFromEnv('NODE_ENV'),
     port: parseInt(requireFromEnv('PORT'), 10),
-    version: packageJson.version
+    version: packageJson.version,
+    databaseUrl: requireFromEnv('DATABASE_URL')
 };
