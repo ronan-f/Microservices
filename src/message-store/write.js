241a8437453e0a8f02b6b1ba5c0af688838546f1
ronan-f <rfegan13@gmail.com>
2020-05-14 21:54:03 -0700

    Handling db version conflict errors with custom error function

--- src/message-store/write.js
+++ src/message-store/write.js
@@ -1,3 +1,5 @@
+const VersionConflictError = require('./version-conflict-error');
+const versionConflictErrorRegex = /^Wrong.*Stream Version: (\d+)\)/;
 const writeFunctionSQL =
     'SELECT message_store.write_message($1, $2, $3, $4, $5, $6)';
 
@@ -16,7 +18,23 @@ function createWrite({ db }) {
             expectedVersion
         ];
 
-        return db.query(writeFunctionSQL, values);
+        return db.query(writeFunctionSQL, values).catch((e) => {
+            const errorMatch = e.message.match(versionConflictErrorRegex);
+
+            if (!errorMatch) throw e;
+
+            const actualVersion = parseInt(errorMatch[1], 10);
+
+            const versionConflictError = new VersionConflictError(
+                streamName,
+                actualVersion,
+                expectedVersion
+            );
+
+            versionConflictError.stack = e.stack;
+
+            throw versionConflictError;
+        });
     };
 }
 
