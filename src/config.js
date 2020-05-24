1a487325f8729bc09560860e81afe33cbca4a74d
ronan-f <rfegan13@gmail.com>
2020-05-23 18:56:38 -0700

    Added messageStore to config

--- src/config.js
+++ src/config.js
@@ -2,18 +2,21 @@ const createKnexClient = require('./knex-client');
 const createHomeApp = require('./app/home');
 const createRecordViewingsApp = require('./app/record-viewings');
 const createPostgresClient = require('./postgres-client');
+const createMessageStore = require('./message-store');
 
 function createConfig({ env }) {
     const db = createKnexClient({ connectionString: env.databaseUrl });
     const postgresClient = createPostgresClient({
         connectionString: env.messageStoreConnectionString
     });
+    const messageStore = createMessageStore({ db: postgresClient });
     const homeApp = createHomeApp({ db });
-    const recordViewingsApp = createRecordViewingsApp({ db });
+    const recordViewingsApp = createRecordViewingsApp({ messageStore });
     return {
         db,
         homeApp,
-        recordViewingsApp
+        recordViewingsApp,
+        messageStore
     };
 }
 
