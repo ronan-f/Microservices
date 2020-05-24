b8de49937de4ad2666c0c2c297236da2cfe44426
ronan-f <rfegan13@gmail.com>
2020-05-23 21:25:32 -0700

    Added basic structure for subscribing to message store

---
+++ src/message-store/subscribe.js
@@ -0,0 +1,7 @@
+const bluebird = require('bluebird');
+const { uuid } = require('uuidv4');
+const category = require('./category');
+
+function configureCreateSubscription({ read, readLastMesage, write }) {}
+
+module.exports = configureCreateSubscription;
