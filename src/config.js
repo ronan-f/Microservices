66c1f41bca66c5ce4e712111b36724e5910e9daf
ronan-f <rfegan13@gmail.com>
2020-05-23 18:55:32 -0700

    Added postgres client to config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,13 @@
 const createKnexClient = require('./knex-client');
 const createHomeApp = require('./app/home');
 const createRecordViewingsApp = require('./app/record-viewings');
+const createPostgresClient = require('./postgres-client');
 
 function createConfig({ env }) {
     const db = createKnexClient({ connectionString: env.databaseUrl });
+    const postgresClient = createPostgresClient({
+        connectionString: env.messageStoreConnectionString
+    });
     const homeApp = createHomeApp({ db });
     const recordViewingsApp = createRecordViewingsApp({ db });
     return {
