6b694244c8d96bfb2fb3ee6e02f89100a696c8fc
ronan-f <rfegan13@gmail.com>
2020-05-23 20:56:06 -0700

    Added a function to increment videos watched counter

--- src/aggregators/home-page.js
+++ src/aggregators/home-page.js
@@ -1,5 +1,27 @@
 function createHandlers({ queries }) {
-    return {};
+    return {
+        VideoViewed: (event) =>
+            queries.incrementVideosWatched(event.globalPosition)
+    };
+}
+
+function incrementVideosWatched(globalPosition) {
+    const queryString = `
+      UPDATE pages
+      SET page_data = jsonb_set(
+        jsonb_set(
+          page_data,
+          '{videosWatched}',
+          ((page_data ->> 'videosWatched')::int + 1)::text::jsonb
+          ),
+          '{lastViewProcessed}',
+          :globalPosition::text::jsonb
+        )
+      WHERE page_name = 'home' AND
+      (page_data->>'lastViewProcessed')::int < :globalPosition
+  `;
+
+    return db.then((client) => client.raw(queryString, { globalPosition }));
 }
 
 function createQueries({ db }) {
