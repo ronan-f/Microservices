978dfbef6839c25efcff951585bd45d38dfbae08
ronan-f <rfegan13@gmail.com>
2020-05-23 18:51:13 -0700

    Added function to record viewing of a video

--- src/app/record-viewings/index.js
+++ src/app/record-viewings/index.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const v4 = require('uuid/v4');
 
-function createActions({}) {}
+function createActions({ messageStore }) {
+    function recordViewing(traceId, videoId, userId) {
+        const viewedEvent = {
+            id: v4(),
+            type: 'VideoViewed',
+            metadata: {
+                traceId,
+                userId
+            },
+            data: {
+                userId,
+                videoId
+            }
+        };
+        const streamName = `viewing-${videoId}`;
+
+        return messageStore.write(streamName, viewedEvent);
+    }
+
+    return { recordViewing };
+}
 
 function createHandlers({ actions }) {
-    function handleRecordViewing(req, res) {
-        await actions.recordViewing(req.context.traceId, req.params.videoId)
-        return res.redirect("/")
+    async function handleRecordViewing(req, res) {
+        await actions.recordViewing(req.context.traceId, req.params.videoId);
+        return res.redirect('/');
     }
 
     return {
         handleRecordViewing
-    }
+    };
 }
 
 function createRecordViewings({}) {
